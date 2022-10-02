2b86920c7e2e58478d8a01eb36494521fa28bedd
Parsa <96871830+Bu64p@users.noreply.github.com>
2022-10-03 00:50:29 +0330

    -> (;

---
+++ heapSorting.h
@@ -0,0 +1,13 @@
+class heapSorting
+{
+private:
+    void heapify(int *, int);
+    void build(int *);
+    void exchange(int *, int, int);
+    int left(int);
+    int right(int);
+    int size;
+
+public:
+    heapSorting(int *, int);
+};
